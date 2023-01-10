cea7a642f787cbddfe5ddee1ad79e49da920a74e
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-01-10 13:35:06 +0000

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""number_of_lines
+"""
+This file contains function that
+writes a string to text file and returns
+number of characters written
 """
 
 
-def number_of_lines(filename=""):
-    """Takes in str filename to read the number of lines
+def write_file(filename="", text=""):
     """
-
-    with open(filename, encoding="utf-8") as readFile:
-        lines = 0
-        while True:
-            line = readFile.readline()
-            if not line:
-                break
-            lines += 1
-        return lines
+    function to write to files
+    """
+    with open(filename, mode="w", encoding="utf-8") as myFile:
+        return (myFile.write(str(text)))
