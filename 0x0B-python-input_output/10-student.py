323397f19a2a6c320bc288c00f94d03154a888aa
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-01-10 14:05:59 +0000

    task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-"""Student
+"""
+This module creates a student class
+with public attributes and retrives
+json dictionary rep
 """
 
 
 class Student:
-    """Contains student data
     """
-
+    student class with public instances
+    """
     def __init__(self, first_name, last_name, age):
+        """
+        instantiation of attr
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves dictionary of Student with conditions to filter
         """
-
-        if attrs == None or type(attrs) != list:
-            return self.__dict__
-        else:
-            temp = {}
-            for elem in attrs:
-                if type(elem) != str:
-                    return self.__dict__
-                if elem in self.__dict__.keys():
-                    temp[elem] = self.__dict__[elem]
-            return temp
+        function returns dict repres
+        of instance
+        """
+        if attrs is None:
+            return (self.__dict__)
+        return ({key: value for key, value in self.__dict__.items()
+                 if key in attrs})
