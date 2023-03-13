200602fb8733fb2caada9ef12a2b079a1120b2ee
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-03-13 19:42:47 +0000

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
+// JS Script
+
 let arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let length = arr.length;
-let i = 0;
-for (i = 0; i < length; i++) {
+for (let i = 0; i < arr.length; i++) {
   console.log(arr[i]);
 }
