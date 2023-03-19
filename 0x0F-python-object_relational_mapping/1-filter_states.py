f405e490d33805b3537007eefe020d0d2380dd33
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-03-19 04:39:28 +0000

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,32 +1,20 @@
 #!/usr/bin/python3
-"""Script that lists all states with a name starting with N"""
-import MySQLdb
-import sys
+"""
+Script to list all states with a name starting with 'N'
+should take 3 arguments username, passwd and db name
+"""
 
-
-def get_states_n():
-    """Takes arguments argv to list from database
-    Only lists with states that start with N
-    Arguments:
-        argv[1]: mysql username
-        argv[2]: mysql password
-        argv[3]: database name
-    """
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
-
-    cur = db.cursor()
-
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
-    rows = cur.fetchall()
-    for i in rows:
-        print(i)
-
-    cur.close()
-    db.close()
 
 if __name__ == "__main__":
-    get_states_n()
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    curr = db.cursor()
+    curr.execute("SELECT * FROM states where name LIKE 'N%'\
+                 ORDER BY states.id ASC")
+    lst = curr.fetchall()
+    for r in lst:
+        if r[1][0] == 'N':
+            print(r)
+    curr.close()
+    db.close()
