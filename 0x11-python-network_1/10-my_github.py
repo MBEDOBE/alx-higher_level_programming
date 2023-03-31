7a19347eba89b83e901a1b849a14a936e2313b2e
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-03-31 21:59:04 +0000

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-import requests
-from sys import argv
 """
 script that  takes in a string and sends a search request to the star wars API
 """
 
 
-if __name__ == "__main__":
-    vals = {"search": argv[1] if len(argv) > 1 else ""}
-    try:
-        req = requests.get('https://swapi.co/api/people/', params=vals).json()
-        count = req.get("count")
-        print("Number of results: {}".format(count))
-        if count > 0:
-            for results in req.get("results"):
-                print(results["name"])
-    except ValueError:
-        print("Not a valid JSON")
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    url = "https://api.github.com/user"
+    response = requests.get(
+        url, auth=requests.auth.HTTPBasicAuth(sys.argv[1], sys.argv[2]))
+    res_dict = response.json()
+    print(res_dict.get('id'))
