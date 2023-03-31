0d7d948da6a296109f2ca080af751b3cf9a3589a
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-03-31 21:51:09 +0000

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-import urllib.request
-import urllib.parse
-from sys import argv
 """
 script that takes in a url and an email, sends a POST request
 to the url with the email as parameter and displays body of the
@@ -10,13 +7,17 @@ response
 
 
 if __name__ == "__main__":
-    """
-    """
-    info = {'email': argv[2]}
-    data = urllib.parse.urlencode(info)
-    data = data.encode('ascii')
-    url = argv[1]
-    reply = urllib.request.Request(url, data)
-    with urllib.request.urlopen(reply) as rep:
-        body = rep.read()
-    print(body.decode(encoding="utf-8"))
+    import urllib.request
+    import urllib.parse
+    import sys
+
+    url_ = sys.argv[1]
+    email_ = {'email': sys.argv[2]}
+    email = urllib.parse.urlencode(email_)
+    email = email.encode('ascii')
+    url_req = urllib.request.Request(url_, email)
+
+    with urllib.request.urlopen(url_req) as response:
+        url_res = response.read()
+    output = url_res.decode('utf-8')
+    print(output)
