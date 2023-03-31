d0d2a5a8a7a211bd77a6dbee9b8914587b422c28
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-03-31 20:49:15 +0000

    Update 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-import urllib.request
-import sys
 """
 Script that sends a request to given url
 displays the value of X-Request-Id variable
@@ -8,5 +6,8 @@ displays the value of X-Request-Id variable
 
 
 if __name__ == "__main__":
+    import sys
+    import urllib.request
     with urllib.request.urlopen(sys.argv[1]) as reply:
-        print(reply.getheader('X-Request-Id'))
+        url_res=reply.info()
+        print(url_res['X-Request-Id'])
