fc2b1f97afd88ad27a6a625c6ff0614a16f96cc4
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-04-25 16:04:45 +0000

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-request.get('https://swapi-api.alx-tools.com/api/films/' + process.argv[2] + '/', function (err, response, body) {
+request.get('https://swapi-api.alx-tools.com/api/films/:id' + process.argv[2] + '/', function (err, response, body) {
   if (err) throw err;
   else if (response.statusCode === 200) {
     console.log(JSON.parse(body).title);
