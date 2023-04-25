3656043d6892daf02a07eb3a78c2b4082b292382
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-04-25 15:26:03 +0000

    task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const lf = require('lf');
+lf.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
