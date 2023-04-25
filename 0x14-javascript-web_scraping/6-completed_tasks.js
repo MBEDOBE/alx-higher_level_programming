ab2f54b18f84aa3fd046443474f4c3353dd9c0ff
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-04-25 16:10:37 +0000

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,15 +1,26 @@
 #!/usr/bin/node
-const request = require('request');
 
-request.get(process.argv[2], function (err, response, body) {
-  if (err) throw err;
-  if (response.statusCode === 200) {
-    let done = {};
-    for (let task of JSON.parse(body)) {
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
       if (task.completed === true) {
-        if (task.userId in done) { done[task.userId] += 1; } else { done[task.userId] = 1; }
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
       }
     }
-    console.log(done);
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });
